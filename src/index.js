93bb3c2eab311f42fc42be55080122e6043180ec
Tussis88 <andreab88@hotmail.it>
2024-08-20 15:35:07 +0200

    setup di base fatto

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./style.css";
+import prova from "./test";
+
+prova();
---
+++ src/test.js
@@ -0,0 +1,7 @@
+export default function prova() {
+  const provaDiv = document.createElement("h1");
+  provaDiv.textContent = "Prova";
+  document.body.appendChild(provaDiv);
+
+  return provaDiv;
+}
